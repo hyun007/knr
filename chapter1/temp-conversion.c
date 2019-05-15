314b074a35863ed03416a7d09f1f26b1c406e5c6
Hyun Jo <hyunjo@gmail.com>
2019-05-15 00:05:42 -0400

    changed to float. page 12

--- chapter1/temp-conversion.c
+++ chapter1/temp-conversion.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int fahr, celcius;
+    float fahr, celcius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -14,8 +14,8 @@ int main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celcius = 5 * (fahr-32) / 9;
-        printf("%3d %6d\n", fahr, celcius);
+        celcius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celcius);
         fahr = fahr + step;
     }
 }
