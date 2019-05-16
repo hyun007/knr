e077c22dcf18326df321e891a86c111c4b48b6f2
Hyun Jo <hyunjo@gmail.com>
2019-05-16 01:49:18 -0400

    added exercise 1-13

---
+++ chapter1/exercises/1-13.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define IN  1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* exercise 1-13: Write a program to print a histogram of the lengths of words
+ * in its input. It is easy to draw the histogram with the bars horizontal; a
+ * vertical orientation is more challenging */
+int main()
+{
+    int c, i, state, wordlen;
+    int histogram[30];
+    
+    state = OUT;
+    wordlen = 0;
+    for (i = 0; i < 30; ++i)
+        histogram[i] = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (state == IN) {
+                ++histogram[wordlen];
+            }
+            state = OUT;
+        }
+        else if (state == OUT) {
+            state = IN;
+            wordlen = 0;
+        } else if (state == IN) {
+            ++wordlen;
+        }
+    }
+
+    for (i = 0; i < 30; ++i) {
+        printf("%2d: ", i+1);
+        int j;
+        for (j = 0; j < histogram[i]; ++j)
+            printf("#");
+        printf("\n");
+    }
+
+    return 0;
+}
