f27965960142176c997e8c0cde5e227e887d88e6
Hyun Jo <hyunjo@gmail.com>
2019-05-17 11:53:36 -0400

    finished debugging exercise 1-18

--- chapter1/exercises/1.18.output.c
+++ chapter1/exercises/1.18.output.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #define MAXLINE 1000    /* maximum input line size */
-
 int get_line(char line[], int maxline);
-
 /* exercise 1-18: write a program to remove trailing blanks and tabs from each
  * line of input, and to delete entirely blank lines. */
 int main()
 {
     int len;                /* current line length */
     char line[MAXLINE];     /* current input line */
-
     while ((len = get_line(line, MAXLINE)) > 0)
         if (len > 1)
             printf("%s", line);
     return 0;
 }
-      
 /* getline: read a line into s, return length */
 int get_line(char s[], int lim)
 {
     int c, i, trim;
-
     for (trim=i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i) {
         s[i] = c;
         if (c != ' ' && c != '\t')
             trim = i;
     }
     if (trim+1 < i) {
-        s[trim+1] = '\n';      
-        s[trim+2] = '\0';     
+        s[trim+1] = '\n';
+        s[trim+2] = '\0';
         return trim+1;
     }
     else {
-        if (c == '\n') {     
+        if (c == '\n') {
             s[i] = c;
             ++i;
         }
@@ -40,4 +35,3 @@ int get_line(char s[], int lim)
         return i;
     }
 }
-     
